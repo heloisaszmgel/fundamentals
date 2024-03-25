44cf11572c4bf5b5b1882fb241c403a3cf7b127e
heloisaszmgel <heloszmgel@gmail.com>
2024-03-24 21:11:56 -0400

    added a few training exercises

---
+++ settingAndSwapping.js
@@ -0,0 +1,13 @@
+// Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa
+
+var myNumber = 42
+var myName = 'Heloisa'
+
+var temp = myNumber
+myNumber = myName
+myName = temp
+
+console.log("myNumber:", myNumber)
+console.log("myName", myName)
+
+
